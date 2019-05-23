29f2240728004aef81d9890809d58c756eb209d9
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-05-24 01:38:13 +0900

    renamed: py_file_resource/__init__.py -> py_base_resource/py_file_resource/__init__.py
    deleted class: BaseResource
      py_base_resource モジュールへ分離。

--- py_base_resource/py_file_resource/__init__.py
+++ py_base_resource/py_file_resource/__init__.py
@@ -2,8 +2,6 @@
 """
 ファイルの操作を管理する。
 """
-from abc import abstractmethod
-import abc
 import codecs
 import os
 import sys
@@ -11,55 +9,8 @@ import sys
 from py_attribute_resource.py_group_resource import GroupResource
 from py_attribute_resource.py_mode_resource import ModeResource
 from py_attribute_resource.py_owner_resource import OwnerResource
+from py_base_resource import BaseResource
 from py_status_resource import StatusResource
-
-
-class BaseResource(object):
-    """
-    基本動作を定義する抽象基底クラス。
-    """
-    __metaclass__ = abc.ABCMeta
-
-    @abstractmethod
-    def create(self):
-        """
-        ファイルまたはディレクトリを作成する。
-        """
-        pass
-
-    @abstractmethod
-    def delete(self):
-        """
-        ファイルまたはディレクトリを削除する。
-        """
-        pass
-
-    @abstractmethod
-    def set_owner(self, owner_name):
-        """
-        ファイルまたはディレクトリの所有者を変更する。
-
-        :param str owner_name: 新しい所有者として設定するユーザの名前を指定する。
-        """
-        pass
-
-    @abstractmethod
-    def set_group(self, group_name):
-        """
-        ファイルまたはディレクトリのグループ所有者を変更する。
-
-        :param str group_name: 新しいグループ所有者として設定するグループの名前を指定する。
-        """
-        pass
-
-    @abstractmethod
-    def set_mode(self, mode):
-        """
-        ファイルまたはディレクトリのパーミッション設定を変更する。
-
-        :param int mode: 新しく設定するパーミッションの値を 4 桁の 8 進数で指定する。
-        """
-        pass
 
 
 class FileResource(StatusResource, BaseResource):
