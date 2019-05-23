f4c2c8b6d870978c8baef8b4a5f8890b208cf993
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-05-23 22:37:34 +0900

    modified: py_file_resource/__main__.py
    new options: owner, group, mode

--- py_file_resource/__main__.py
+++ py_file_resource/__main__.py
@@ -41,6 +41,18 @@ def main():
     .. encoding <encoding>
 
         ファイル読込時に使用する文字エンコーディングを指定する。
+
+    .. option:: owner <user_name>
+
+        ファイルの所有者を変更する。
+
+    .. option:: group <group_name>
+
+        ファイルのグループ所有者を変更する。
+
+    .. option:: mode <mode>
+
+        ファイルのパーミッション設定を変更する。
     """
     parser = argparse.ArgumentParser(
         prog="py_file_resource", description="ファイルを作成または削除します。")
@@ -55,6 +67,10 @@ def main():
     parser.add_argument("--read", action="store_true",
                         default=False, help="ファイルの内容を出力します。")
     parser.add_argument("--encoding", metavar="encoding", help="文字コードを指定します。")
+    parser.add_argument("--owner", metavar="user_name", help="ファイルの所有者を変更します。")
+    parser.add_argument("--group", metavar="group_name",
+                        help="ファイルのグループ所有者を変更します。")
+    parser.add_argument("--mode", metavar="mode", help="ファイルのパーミッション設定を変更します。")
 
     args = parser.parse_args()
     fr = FileResource(args.path)
@@ -83,6 +99,24 @@ def main():
         fr.set_content(args.update)
     elif args.append:
         fr.append_content(args.append)
+
+    if fr.code == 1:
+        sys.exit(fr.code)
+
+    if args.owner:
+        fr.set_owner(args.owner)
+
+    if fr.code == 1:
+        sys.exit(fr.code)
+
+    if args.group:
+        fr.set_group(args.group)
+
+    if fr.code == 1:
+        sys.exit(fr.code)
+
+    if args.mode:
+        fr.set_mode(int(args.mode, 8))
 
     sys.exit(fr.code)
 
