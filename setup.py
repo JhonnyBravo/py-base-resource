aa1f37785cb42a144dbccadb4448a76fded61576
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-05-24 21:57:55 +0900

    modified: setup.py
    new attribute: entry_points

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name="py-base-resource",
-    version="1.0.0",
+    version="1.0.1",
     description="Python module for file and directory management",
     author="Jhonny Bravo",
     author_email="sanfranceshika5@gmail.com",
@@ -23,6 +23,12 @@ setup(
         "git+https://github.com/JhonnyBravo/py-status-resource.git#egg=py_status_resource",
         "git+https://github.com/JhonnyBravo/py-attribute-resource.git#egg=py_attribute_resource"
     ],
+    entry_points={
+        "console_scripts": [
+            "py_file_resource=py_base_resource.script.py_file_resource:main",
+            "py_directory_resource=py_base_resource.script.py_directory_resource:main"
+        ]
+    },
     long_description=long_description,
     classifiers=[
         "Programming Language :: Python",
